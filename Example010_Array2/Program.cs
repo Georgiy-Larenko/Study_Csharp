6cb4a21ef965ee6c8c89c2ba806e6e17e8fd12bf
Georgiy <goshalar@gmail.com>
2022-08-31 22:36:54 +0500

    Add Example 10 Array 2

---
+++ Example010_Array2/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 13, 22, 34, 45, 35, 76, 227, 98, 139 };
+
+int n = array.Length; // Обозначение длинны массива
+int find = 22;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find) // == это совпадение(равенство)
+    {
+        Console.WriteLine(index);
+        break; // Прерывание цикла если условие выполняется (если несколько похожих значений)
+    }
+    index++; // index = index + 1;
+}
